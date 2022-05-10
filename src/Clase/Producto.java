4dc84ddec93213fc77a4e84fee395afe144d3b10
PacoAlbi <francisco.albinana@iesnervion.es>
2022-05-10 10:53:56 +0200

    10/05/2022 Clase

--- src/Clase/Producto.java
+++ src/Clase/Producto.java
@@ -11,7 +11,9 @@ public class Producto {
 
     public void setDescripcion(String descripcion) throws Exception {
         if (descripcion.length()<1 || descripcion.length()>50){
-            throw new Exception("El tamaño no está en el rango permitido. Mínimo 1 máximo 50."){};
+            throw new Exception(
+                    String.format("El tamaño no está en el rango permitido. Mínimo 1 máximo 50 y tiene %d", descripcion.length())
+            );
         }
         this.descripcion = descripcion;
     }
@@ -22,7 +24,8 @@ public class Producto {
 
     public void setPrecio(double precio) throws Exception {
         if (precio<=0){
-            throw new Exception("El precio tiene que ser mayor que 0.0"){};
+            throw new Exception(
+                    String.format("El precio tiene que ser mayor que 0.0 y es %f", precio));
         }
         this.precio = precio;
     }
